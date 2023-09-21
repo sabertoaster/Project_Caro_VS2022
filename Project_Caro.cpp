4a294914924cf94a9a5c7f665f3e518a0bfdbe17
sabertoaster <62453787+sabertoaster@users.noreply.github.com>
2023-09-21 10:56:59 +0700

    Customize height width console

--- Project_Caro.cpp
+++ Project_Caro.cpp
@@ -12,6 +12,53 @@ void FixConsoleWindow()
     LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
     style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
     SetWindowLong(consoleWindow, GWL_STYLE, style);
+
+
+    // Define the custom height and width
+    int customWidth = 80;  // Replace with your desired width
+    int customHeight = 40; // Replace with your desired height
+    // Get the handle to the console window
+    //HWND consoleWindow = GetConsoleWindow();
+    // Create a SMALL_RECT structure to define the new console size
+    SMALL_RECT rect;
+    rect.Left = 0;
+    rect.Top = 0;
+    rect.Right = customWidth - 1;
+    rect.Bottom = customHeight - 1;
+    // Set the console screen buffer size to match the new size
+    COORD size;
+    size.X = customWidth;
+    size.Y = customHeight;
+    // Set the console window size and screen buffer size
+    SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), TRUE, &rect);
+    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), size);
+
+
+
+    // get handle to the console window
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    // retrieve screen buffer info
+    CONSOLE_SCREEN_BUFFER_INFO scrBufferInfo;
+    GetConsoleScreenBufferInfo(hOut, &scrBufferInfo);
+    // current window size
+    short winWidth = scrBufferInfo.srWindow.Right - scrBufferInfo.srWindow.Left + 1;
+    short winHeight = scrBufferInfo.srWindow.Bottom - scrBufferInfo.srWindow.Top + 1;
+    // current screen buffer size
+    short scrBufferWidth = scrBufferInfo.dwSize.X;
+    short scrBufferHeight = scrBufferInfo.dwSize.Y;
+    // to remove the scrollbar, make sure the window height matches the screen buffer height
+    COORD newSize;
+    newSize.X = scrBufferWidth;
+    newSize.Y = winHeight;
+    // set the new screen buffer dimensions
+    int Status = SetConsoleScreenBufferSize(hOut, newSize);
+    if (Status == 0)
+    {
+        cout << "SetConsoleScreenBufferSize() failed! Reason : " << GetLastError() << endl;
+        exit(Status);
+    }
+
+
 }
 
 // View
@@ -41,7 +88,21 @@ COORD GetConsoleCursorPosition(HANDLE hConsoleOutput) //return current pointer c
 int main()
 {
     FixConsoleWindow();
-    cout << "Hello World";
+    cout << "   _________    ____  ____\n";
+    cout << "  / ____/   |  / __ \\/ __ \\ \n";
+    cout << " / /   / /| | / /_/ / / / / \n";
+    cout << "/ /___/ ___ |/ _, _/ /_/ / \n";
+    cout << "\\____/_/  |_/_/ |_|\\____/  \n";
+    /*
+   _________    ____  ____ 
+  / ____/   |  / __ \/ __ \
+ / /   / /| | / /_/ / / / /
+/ /___/ ___ |/ _, _/ /_/ / 
+\____/_/  |_/_/ |_|\____/  
+
+    */
+
+
     while (true)
     {
         if (_kbhit())
