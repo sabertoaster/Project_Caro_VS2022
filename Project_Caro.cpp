5a665c37e6b73dd16587f7980716b36fd2052c33
Trung Kiet <huynhtrungkiet09032005@gmail.com>
2023-09-21 16:36:58 +0700

    ADD header

--- Project_Caro.cpp
+++ Project_Caro.cpp
@@ -2,41 +2,11 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include "thu_vien.h"
 using namespace std;
 
 typedef long long ll;
 
-void FixConsoleWindow()
-{
-    HWND consoleWindow = GetConsoleWindow();
-    LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
-    style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
-    SetWindowLong(consoleWindow, GWL_STYLE, style);
-}
-
-// View
-void GotoXY(int x, int y)
-{
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-COORD GetConsoleCursorPosition(HANDLE hConsoleOutput) //return current pointer coordinate in console windows
-{
-    CONSOLE_SCREEN_BUFFER_INFO cbsi;
-    if (GetConsoleScreenBufferInfo(hConsoleOutput, &cbsi))
-    {
-        return cbsi.dwCursorPosition;
-    }
-    else
-    {
-        // The function failed. Call GetLastError() for details.
-        COORD invalid = { 0, 0 };
-        return invalid;
-    }
-}
-// View
 
 int main()
 {
---
+++ thu_vien.h
@@ -0,0 +1,34 @@
+#pragma once
+#include<window.h>
+#include<stdio.h>
+void FixConsoleWindow()
+{
+    HWND consoleWindow = GetConsoleWindow();
+    LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+    style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+    SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+
+// View
+void GotoXY(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+COORD GetConsoleCursorPosition(HANDLE hConsoleOutput) //return current pointer coordinate in console windows
+{
+    CONSOLE_SCREEN_BUFFER_INFO cbsi;
+    if (GetConsoleScreenBufferInfo(hConsoleOutput, &cbsi))
+    {
+        return cbsi.dwCursorPosition;
+    }
+    else
+    {
+        // The function failed. Call GetLastError() for details.
+        COORD invalid = { 0, 0 };
+        return invalid;
+    }
+}
+// View
